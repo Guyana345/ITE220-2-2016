c1c8a7170a7b16b274160742f8b018d7a25e630c
Tafadzwa Makuyana <tafadzwamakuyana@Tafadzwas-MacBook-Pro.local>
2016-11-29 12:02:21 +0700

    week 3 added (test 2)

---
+++ Week 3/js/shop.js
@@ -0,0 +1,7 @@
+var customerName = "Taf";
+var price = "10";
+var quantity = 2;
+var totalPrice = price * quantity;
+
+var customerElement = document.getElementById("sp");
+customerElement.textContent = customerName;
