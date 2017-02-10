1228904a14d5e867bd78d56a935bf9764d8d8ff9
Tafadzwa Makuyana <tafadzwamakuyana@Tafadzwas-MacBook-Pro.local>
2017-02-10 14:37:31 +0700

    database files

---
+++ GroupProject_restaurant/database_Files/users.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `ite220_users`
+//
+
+// `ite220_users`.`users`
+$users = array(
+  array('ID' => '1','USERNAME' => 'admin','PASSWORD' => '81dc9bdb52d04dc20036dbd8313ed055','FIRSTNAME' => 'Mintra Ruensuk'),
+  array('ID' => '2','USERNAME' => 'hacker','PASSWORD' => '81dc9bdb52d04dc20036dbd8313ed055','FIRSTNAME' => 'Hacker'),
+  array('ID' => '19','USERNAME' => 'daominh','PASSWORD' => 'fcea920f7412b5da7be0cf42b8c93759','FIRSTNAME' => 'dao'),
+  array('ID' => '20','USERNAME' => 'tee1','PASSWORD' => '3b712de48137572f3849aabd5666a4e3','FIRSTNAME' => 'tee')
+);
